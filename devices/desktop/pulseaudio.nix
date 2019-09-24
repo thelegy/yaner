d4b12988e67d6ac1392008a821914a4193bd0797
Jan Beinke <git@janbeinke.com>
2019-09-24 12:00:28 +0200

    Add temporary hacky mediakey support for pulseaudio

--- devices/desktop/pulseaudio.nix
+++ devices/desktop/pulseaudio.nix
@@ -9,4 +9,18 @@
     ncpamixer
   ];
 
+  services.acpid = let
+    volumeStep = 5;
+    pauser = "beinke";
+    pamixer = commands: ''
+      /run/wrappers/bin/su ${pauser} -c 'XDG_RUNTIME_DIR=/run/user/$(${pkgs.coreutils}/bin/id -u ${pauser}) ${pkgs.pamixer}/bin/pamixer ${commands}'
+    '';
+  in {
+    enable = true;
+    handlers.volmute = { event = "button/mute";       action = pamixer "--toggle-mute"; };
+    handlers.voldown = { event = "button/volumedown"; action = pamixer "--decrease ${toString volumeStep} --unmute"; };
+    handlers.volup   = { event = "button/volumeup";   action = pamixer "--increase ${toString volumeStep} --unmute"; };
+    handlers.micmute = { event = "button/f20";        action = pamixer "--default-source --toggle-mute"; };
+  };
+
 }
