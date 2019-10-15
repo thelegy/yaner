0414c086a7370ac3ada6764612ad3953d88e692e
Jan Beinke <git@janbeinke.com>
2019-10-15 10:45:49 +0200

    Fixup some links to templates. Oops

--- hosts/th1/default.nix
+++ hosts/th1/default.nix
@@ -4,7 +4,7 @@
 
   imports = [
     ./hardware-configuration.nix
-    ../templates/laptop
+    ../../templates/laptop
   ];
 
 
--- templates/laptop/default.nix
+++ templates/laptop/default.nix
@@ -3,8 +3,8 @@
 {
 
   imports = [
-    ./workstation
-    ./amnesia
+    ../workstation
+    ../amnesia
   ];
 
   # Enable brightness switches
