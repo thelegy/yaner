b8097d0dddb2ded98d2ccd38a58bdb80f4e3adfd
Jan Beinke <git@janbeinke.com>
2020-11-22 23:10:31 +0100

    Fix nixroute config not working anymore

--- machines/roborock/configuration.nix
+++ machines/roborock/configuration.nix
@@ -33,6 +33,7 @@ in {
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMCC4cFL1xcZOsIzXg1b/M4b89ofMKErNhg9s+0NdBVC beinke@th1"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPMkJA05G5ozn/pYRxrbQbk8lRynG4jH5LG1fua0Jo7c root@th1"
     ];
   };
 
