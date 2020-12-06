9c994d1b31c00f4f7c257723f727c59df2cd7d3a
Jan Beinke <git@janbeinke.com>
2020-12-06 18:25:21 +0100

    Use gamma control for volume steps

--- layers/desktop/pulseaudio.nix
+++ layers/desktop/pulseaudio.nix
@@ -13,7 +13,8 @@
   ];
 
   services.acpid = let
-    volumeStep = 5;
+    volumeStep = 1;
+    gammaCorrection = 3;
     pauser = "beinke";
     pamixer = commands: ''
       /run/wrappers/bin/su ${pauser} -c 'XDG_RUNTIME_DIR=/run/user/$(${pkgs.coreutils}/bin/id -u ${pauser}) ${pkgs.pamixer}/bin/pamixer ${commands}'
@@ -21,8 +22,8 @@
   in {
     enable = true;
     handlers.volmute = { event = "button/mute";       action = pamixer "--toggle-mute"; };
-    handlers.voldown = { event = "button/volumedown"; action = pamixer "--decrease ${toString volumeStep} --unmute"; };
-    handlers.volup   = { event = "button/volumeup";   action = pamixer "--increase ${toString volumeStep} --unmute"; };
+    handlers.voldown = { event = "button/volumedown"; action = pamixer "--gamma ${toString gammaCorrection} --decrease ${toString volumeStep} --unmute"; };
+    handlers.volup   = { event = "button/volumeup";   action = pamixer "--gamma ${toString gammaCorrection} --increase ${toString volumeStep} --unmute"; };
     handlers.micmute = { event = "button/f20";        action = pamixer "--default-source --toggle-mute"; };
   };
 
