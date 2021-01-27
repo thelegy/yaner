074ebd12cd21aa7bb5c9ab3b07ec195d0fbc2380
Jan Beinke <git@janbeinke.com>
2021-01-27 16:05:46 +0100

    Cleanup whitespaces

--- flake.nix
+++ flake.nix
@@ -2,22 +2,27 @@
   description = "Yet Another Nix Expression Repository";
 
   inputs = {
+
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
     homemanager = {
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     queezle-dotfiles = {
       url = gitlab:jens/dotfiles?host=git.c3pb.de;
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixpkgs-stable.url = github:NixOS/nixpkgs/nixos-20.09;
+
   };
 
   outputs = inputs@{self, nixpkgs, ...}: let
     outputs = rec {
-      utils = import (self + "/utils") nixpkgs.lib; 
+
+      utils = import (self + "/utils") nixpkgs.lib;
 
       overlay = import ./pkgs;
       nixosModules = utils.findNixosModules {
@@ -26,6 +31,7 @@
       };
 
       nixosConfigurations = import self meta;
+
     };
     meta = inputs // outputs;
   in outputs;
