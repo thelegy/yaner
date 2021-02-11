f42957d8fe3ce62848a2dcc14e711bc6e730122c
Jan Beinke <git@janbeinke.com>
2021-02-11 13:29:51 +0100

    Add basic networking config of roborock

--- machines/roborock/configuration.nix
+++ machines/roborock/configuration.nix
@@ -1,4 +1,5 @@
-{ config, options, pkgs, channels, ... }:
+{ lib, config, options, pkgs, ... }:
+with lib;
 
 {
 
@@ -12,6 +13,73 @@
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
 
+  networking.useDHCP = false;
+
+  systemd.network = {
+    enable = true;
+    netdevs.internal = {
+      netdevConfig = {
+        Name = "internal";
+        Kind = "vlan";
+      };
+      vlanConfig.Id = 42;
+    };
+    networks.internal = {
+      name = "internal";
+      address = [ "10.0.16.1/22" ];
+      networkConfig.IPForward = true;
+      extraConfig = ''
+        [Network]
+        DHCPv6PrefixDelegation = yes
+        IPv6SendRA = yes
+
+        [IPv6SendRA]
+        RouterLifetimeSec = 1800
+
+        [DHCPv6PrefixDelegation]
+        SubnetId = 2
+        Token = ::1
+
+        [CAKE]
+        Bandwidth =
+        '';
+    };
+    netdevs.uplink = {
+      netdevConfig = {
+        Name = "uplink";
+        Kind = "vlan";
+      };
+      vlanConfig.Id = 1;
+    };
+    networks.uplink = {
+      name = "uplink";
+      DHCP = "yes";
+      networkConfig = {
+        IPForward = true;
+        IPv6AcceptRA = true;
+        IPv6PrivacyExtensions = true;
+        #IPv6PrefixDelegation = "yes";
+      };
+      extraConfig = ''
+        [CAKE]
+        Bandwidth = 35M
+
+        [DHCPv6]
+        ForceDHCPv6PDOtherInformation = yes
+      '';
+    };
+    networks.eth0 = {
+      name = "eth0";
+      vlan = [
+        "internal"
+        "uplink"
+      ];
+      extraConfig = ''
+        [CAKE]
+        Bandwidth =
+      '';
+    };
+  };
 
   users.users.nix = {
     isNormalUser = true;
