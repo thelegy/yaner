96e35d29c3632039bfbfb3b257b0a93b0f7fd586
Jan Beinke <git@janbeinke.com>
2021-12-03 14:27:52 +0100

    Update nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,8 +6,6 @@ with self;
 
   inxi-full = inxi.override { withRecommends = true; };
 
-  spotifyd = super.spotifyd.override {rustPackages=self.rustPackages_1_45;};
-
   media_volume = callPackage ./media_volume.nix {};
 
   mpv_autospeed = ./mpv_autospeed.lua;
