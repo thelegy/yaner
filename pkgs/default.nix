c0c669129c3d44d560430c5d47ad25ff1142a7a1
Jan Beinke <git@janbeinke.com>
2022-03-28 16:00:07 +0200

    Update nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,12 +4,12 @@ with final;
 
 {
 
+  fuzzel = flakes.nixpkgs-staging-next.legacyPackages.${system}.fuzzel;
+
   inxi-full = inxi.override { withRecommends = true; };
 
   media_volume = callPackage ./media_volume.nix {};
 
   mpv_autospeed = ./mpv_autospeed.lua;
-
-  kicad = flakes.nixpkgs-staging-next.legacyPackages.${system}.kicad;
 
 }
