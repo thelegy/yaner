9fdec17648943b40101a79f4405cc83359aa1150
Jan Beinke <git@janbeinke.com>
2023-01-22 19:26:56 +0100

    machine/migrator: Fix longstanding evaluation error

--- machines/migrator/default.nix
+++ machines/migrator/default.nix
@@ -4,7 +4,14 @@ mkMachine {} ({ lib, pkgs, config, ... }: with lib; let
 
   networkInterface = "enp1s0";
 
+  installDisk = "/dev/disk/by-id/ata-SanDisk_SDSSDP128G_141350402051";
+  efiId = "CBC7-7164";
+  luksUuid = "101359d3-6a37-4ad7-be51-e8335dd4046b";
+  swapUuid = "d0b261e3-eb15-4778-8b5a-f9804a6ae02e";
+
 in {
+
+  system.stateVersion = "22.11";
 
   imports = [
     ./hardwareConfiguration.nix
@@ -24,13 +31,24 @@ in {
     interface = networkInterface;
   };
 
-  wat.installer = {
-    enable = true;
-    installDisk = "/dev/disk/by-id/ata-SanDisk_SDSSDP128G_141350402051";
-    efiId = "CBC7-7164";
-    luksUuid = "101359d3-6a37-4ad7-be51-e8335dd4046b";
-    swapUuid = "d0b261e3-eb15-4778-8b5a-f9804a6ae02e";
+  boot.initrd.luks.devices.migrator = {
+    device = "/dev/disk/by-uuid/${luksUuid}";
+    allowDiscards = true;
   };
+
+  fileSystems."/" = {
+    device = "/dev/mapper/vg_migrator-system";
+    fsType = "bcachefs";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/${efiId}";
+    fsType = "vfat";
+  };
+  swapDevices = [
+    {
+      device = "/dev/mapper/vg_migrator-swap";
+    }
+  ];
 
   services.logind.lidSwitch = "ignore";
 
