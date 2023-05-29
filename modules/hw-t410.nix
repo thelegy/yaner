8d4c181f548ca764b1ee204c93f1d546165c8876
Jan Beinke <git@janbeinke.com>
2023-05-29 08:59:13 +0200

    module/hw-t410: Switch to explicit configuration

--- modules/hw-t410.nix
+++ modules/hw-t410.nix
@@ -9,6 +9,50 @@ mkTrivialModule {
   hardware.cpu.intel.updateMicrocode = mkDefault true;
 
   # Enable silent fan profile
-  services.thinkfan.enable = true;
+  services.thinkfan = {
+    enable = true;
+    sensors = [];
+    levels = [];
+    extraArgs = [ "-b-9" ];
+    settings = {
+      sensors = [
+        {
+          hwmon = "/sys/class/hwmon";
+          name = "coretemp";
+          indices = [ 2 3 ];
+        }
+        {
+          hwmon = "/sys/class/hwmon";
+          name = "acpitz";
+          indices = [ 1 ];
+        }
+      ];
+      levels = [
+        {
+          speed = 0;
+          upper_limit = [50 50 60];
+        }
+        {
+          speed = 2;
+          lower_limit = [ 45 45 55 ];
+          upper_limit = [ 55 55 65 ];
+        }
+        {
+          speed = 4;
+          lower_limit = [ 50 50 58 ];
+          upper_limit = [ 60 60 68 ];
+        }
+        {
+          speed = 7;
+          lower_limit = [ 55 55 60 ];
+          upper_limit = [ 75 75 85 ];
+        }
+        {
+          speed = "level disengaged";
+          lower_limit = [ 70 70 80 ];
+        }
+      ];
+    };
+  };
 
 }
