02d81fc4f71d7cbc1742fe15c0a37e03f016861e
Jan Beinke <git@janbeinke.com>
2023-08-04 10:25:40 +0200

    machine/y: Init new machine

---
+++ machines/y/default.nix
@@ -0,0 +1,53 @@
+{ mkMachine, ... }:
+
+mkMachine {} ({ pkgs, config, ... }: let
+
+  networkInterface = "enp2s0";
+  macAddress = "7c:10:c9:b8:53:6d";
+
+in {
+
+  system.stateVersion = "22.11";
+
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  wat.installer.btrfs = {
+    enable = true;
+    installDisk = "/dev/disk/by-id/nvme-Samsung_SSD_980_PRO_1TB_S5GXNX0W493233T";
+    swapSize = "8GiB";
+  };
+
+  hardware.cpu.amd.updateMicrocode = true;
+  powerManagement.cpuFreqGovernor = "schedutil";
+
+  wat.thelegy.base.enable = true;
+  wat.thelegy.firewall.enable = true;
+
+  networking.useDHCP = false;
+
+  systemd.network = {
+    enable = true;
+    netdevs.br0 = {
+      netdevConfig = {
+        Name = "br0";
+        Kind = "bridge";
+        MACAddress = macAddress;
+      };
+    };
+    networks.${networkInterface} = {
+      name = "${networkInterface}";
+      bridge = [ "br0" ];
+    };
+    networks.br0 = {
+      name = "br0";
+      DHCP = "yes";
+      extraConfig = ''
+        [CAKE]
+        Bandwidth =
+      '';
+    };
+  };
+
+})
