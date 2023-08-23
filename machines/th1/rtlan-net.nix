909a51d5f07ad8312dea8532843ea7088ca83e46
Jan Beinke <git@janbeinke.com>
2023-08-23 09:49:45 +0200

    module/rl-lan: Switch to different ip range

--- machines/th1/rtlan-net.nix
+++ machines/th1/rtlan-net.nix
@@ -1,4 +1,4 @@
 {
-  address = "100.113.65.4/24";
+  address = "100.115.93.4/24";
   publicKey = "e3l3OIrdMr1q/lTqpzrpTgjGTu2x+7msYcVIetx3000=";
 }
--- machines/y/rtlan-net.nix
+++ machines/y/rtlan-net.nix
@@ -1,5 +1,5 @@
 {
-  address = "100.113.65.2/24";
+  address = "100.115.93.2/24";
   publicKey = "M64+7/GOhTpfZfHXMj7MEJ/1NoQ5MADL9oX/tuqwRjU=";
   endpoint = "192.168.1.3";
 }
