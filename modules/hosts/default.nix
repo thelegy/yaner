8d9fa62a0bd5b358a864e598be9724c2a1e6eb7c
Jan Beinke <git@janbeinke.com>
2023-08-28 15:30:33 +0200

    module/hosts: Move module configuration to json

---
+++ modules/hosts/default.nix
@@ -0,0 +1,44 @@
+{ mkModule
+, config
+, lib
+, liftToNamespace
+, ...
+}:
+with lib;
+mkModule {
+
+  options = cfg: liftToNamespace {
+    hosts = mkOption {
+      type = types.attrsOf (types.submodule ({ name, config, ... }: {
+        options = {
+          hostName = mkOption { type = types.str; };
+          addresses = mkOption { type = types.listOf types.str; default = [ ]; };
+          publicKey = mkOption { type = types.nullOr types.str; default = null; };
+        };
+        config = { hostName = name; };
+      }));
+      default = { };
+    };
+  };
+
+  config = cfg:
+    (liftToNamespace { hosts = importJSON ./hosts.json; })
+    //
+    {
+      networking.hosts = mkMerge
+        (mapAttrsToList
+          (_: host: genAttrs host.addresses (_: [ host.hostName ]))
+          cfg.hosts);
+      programs.ssh.knownHosts =
+        mapAttrs
+          (_: host: {
+            publicKey = host.publicKey;
+            hostNames = [
+              host.hostName
+              "${host.hostName}.${config.networking.domain}"
+            ] ++ host.addresses;
+          })
+          (filterAttrs (_: host: !isNull host.publicKey) cfg.hosts);
+    };
+
+}
