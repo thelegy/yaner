3ff276d4878b2f22d51c6a5f641020f55b804e61
Jan Beinke <git@janbeinke.com>
2023-10-06 11:01:38 +0200

    machine/y: Add nginx

--- machines/y/default.nix
+++ machines/y/default.nix
@@ -34,6 +34,7 @@ in {
   wat.thelegy.base.enable = true;
   wat.thelegy.firewall.enable = true;
   wat.thelegy.hass.enable = true;
+  wat.thelegy.nginx.enable = true;
   wat.thelegy.rtlan-net.enable = true;
   wat.thelegy.tailscale.enable = true;
 
