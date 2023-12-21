9472d85f0ef5691cd72579ac6694d822feb0e13b
Jan Beinke <git@janbeinke.com>
2023-12-21 23:30:38 +0100

    pkgs: Apply nix formatter

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,14 +1,10 @@
 flakes: final: prev:
-
-with final;
-
-{
-
+with final; {
   bs-oneclick = callPackage ./bs-oneclick.nix {};
 
   cs-firewall-bouncer = callPackage ./cs-firewall-bouncer.nix {};
 
-  inxi-full = inxi.override { withRecommends = true; };
+  inxi-full = inxi.override {withRecommends = true;};
 
   itd = callPackage ./itd.nix {};
 
@@ -21,10 +17,11 @@ with final;
       rev = "9738110d48c2e38f4d03d12839684b06abf34244";
       hash = "sha256-Yq+H4cikZb58JzZP7CPJH/ean78pGMWJDuGLZgI06Eo=";
     };
-  in on-demand-shell {
-    name = "cadquery";
-    installable = "path://${cq-flake}";
-  };
+  in
+    on-demand-shell {
+      name = "cadquery";
+      installable = "path://${cq-flake}";
+    };
 
   lego =
     if lib.versionOlder prev.lego.version "4.9.1"
@@ -39,30 +36,30 @@ with final;
 
   nixGL = final.nixgl.nixGLCommon final.nixgl.nixGLIntel;
 
-  on-demand-shell =
-    { name
-    , scriptName ? "launch-${name}"
-    , installable ? null
-    , args ? ""
-    }: writeShellScriptBin scriptName ''
+  on-demand-shell = {
+    name,
+    scriptName ? "launch-${name}",
+    installable ? null,
+    args ? "",
+  }:
+    writeShellScriptBin scriptName ''
       set -euo pipefail
       readonly gc_root_dir="''${XDG_DATA_HOME:-$HOME/.local/share}/nix-on-demand"
       mkdir -p "$gc_root_dir"
       ${
         lib.optionalString
-          (!isNull installable)
-          ''nix build --out-link "$gc_root_dir/${name}" '${installable}' ''
+        (!isNull installable)
+        ''nix build --out-link "$gc_root_dir/${name}" '${installable}' ''
       }
       exec nix shell "$gc_root_dir/${name}" ${args} "$@"
     '';
 
-  preprocess-cancellation =
-    python3Packages.preprocess-cancellation.overrideAttrs (orig:{
-      postPatch = ''
+  preprocess-cancellation = python3Packages.preprocess-cancellation.overrideAttrs (orig: {
+    postPatch = ''
       ${orig.postPatch}
       sed -i 's/\[tool.poetry.scripts\]/[project.scripts]/' -i pyproject.toml
-      '';
-    });
+    '';
+  });
 
   qbar = flakes.qbar.packages.${system}.qbar;
 
