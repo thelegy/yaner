3d25cfd6e1525469aabd2a4b40c22e6c37c89387
Jan Beinke <git@janbeinke.com>
2023-12-26 20:46:04 +0100

    module/monitoring: Add log output to loki

--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -16,6 +16,10 @@ in
           type = types.str;
           default = "https://prometheus.0jb.de:9090/api/v1/write";
         };
+        lokiUrl = mkOption {
+          type = types.nullOr types.str;
+          default = "https://loki.0jb.de:3100/loki/api/v1/push";
+        };
         scrapeConfigs = mkOption {
           type = types.attrsOf yaml.type;
           default = {};
@@ -64,6 +68,42 @@ in
               instance = config.networking.hostName;
               enable_collectors = ["systemd"];
             };
+          };
+          logs = mkIf (! isNull cfg.lokiUrl) {
+            positions_directory = "\${STATE_DIRECTORY}/logs-positions";
+            configs = [
+              {
+                name = "journal";
+                clients = [{url = cfg.lokiUrl;}];
+                scrape_configs = [
+                  {
+                    job_name = "journal";
+                    journal = {
+                      max_age = "12h";
+                      labels = {
+                        job = "journal";
+                        host = config.networking.hostName;
+                      };
+                    };
+                    relabel_configs = let
+                      label = target: source: {
+                        source_labels = ["__journal_${source}"];
+                        target_label = target;
+                      };
+                    in [
+                      (label "cmdLine" "_cmdline")
+                      (label "pid" "_pid")
+                      (label "priority" "priority")
+                      (label "slice" "_systemd_slice")
+                      (label "syslogFacility" "syslog_facility")
+                      (label "syslogIdentifier" "syslog_identifier")
+                      (label "uid" "_uid")
+                      (label "unit" "_systemd_unit")
+                    ];
+                  }
+                ];
+              }
+            ];
           };
         };
       };
