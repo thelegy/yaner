c9de0fbb0799feb807dd9640af884d4eb0171009
Jan Beinke <git@janbeinke.com>
2024-02-27 08:51:23 +0100

    module/crowdsec: Reformat file

--- modules/crowdsec.nix
+++ modules/crowdsec.nix
@@ -94,157 +94,159 @@ in
 
         "${configDir}/hub".source = hub;
       };
-    in (liftToNamespace {
-      parsers = [
-        "crowdsecurity/geoip-enrich"
-        "crowdsecurity/sshd-logs"
-        "crowdsecurity/syslog-logs"
-      ];
-      scenarios = [
-        "crowdsecurity/ssh-bf"
-        "crowdsecurity/ssh-slow-bf"
-      ];
-      journalctlFilters = [
-        "_SYSTEMD_UNIT=sshd.service"
-      ];
-    }) // {
-      environment.systemPackages = [pkgs.crowdsec];
-      environment.etc = etcConfig;
+    in
+      (liftToNamespace {
+        parsers = [
+          "crowdsecurity/geoip-enrich"
+          "crowdsecurity/sshd-logs"
+          "crowdsecurity/syslog-logs"
+        ];
+        scenarios = [
+          "crowdsecurity/ssh-bf"
+          "crowdsecurity/ssh-slow-bf"
+        ];
+        journalctlFilters = [
+          "_SYSTEMD_UNIT=sshd.service"
+        ];
+      })
+      // {
+        environment.systemPackages = [pkgs.crowdsec];
+        environment.etc = etcConfig;
 
-      users.groups.crowdsec = {};
+        users.groups.crowdsec = {};
 
-      sops.secrets.${cfg.sopsLapiCredentialsFile} = {
-        format = "yaml";
-        mode = "0640";
-        group = "crowdsec";
-        restartUnits = ["crowdsec.service" "crowdsec-lapi.service"];
-      };
-
-      sops.secrets.${cfg.sopsBouncerCredentialsFile} = {
-        format = "yaml";
-        mode = "0640";
-        group = "crowdsec";
-        restartUnits = ["cs-firewall-bouncer.service"];
-      };
-
-      systemd.services.crowdsec-agent = let
-        preStartScript = pkgs.writeScript "crowdsec-agent-pre-start" ''
-          #!/bin/sh
-          set -e
-
-          rm -rf /etc/crowdsec/{parsers,scenarios}/* || true
-          # mkdir -p /etc/crowdsec/{parsers,scenarios}
-
-          ${pkgs.crowdsec}/bin/cscli parsers install ${escapeShellArgs cfg.parsers}
-
-          ${pkgs.crowdsec}/bin/cscli scenarios install ${escapeShellArgs cfg.scenarios}
-        '';
-      in {
-        description = "Crowdsec agent";
-        wantedBy = ["multi-user.target"];
-
-        restartTriggers = [(builtins.toJSON etcConfig)];
-
-        serviceConfig = {
-          Type = "notify";
-          ExecStartPre = [
-            "+${pkgs.coreutils}/bin/mkdir -p /etc/crowdsec/{parsers,scenarios}"
-            "!${preStartScript}"
-          ];
-          ExecStart = "${pkgs.crowdsec}/bin/crowdsec";
-          Environment = "LC_ALL=C LANG=C";
-          Restart = "always";
-          RestartSec = 10;
-          DynamicUser = true;
-          ReadWritePaths = [
-            "/etc/crowdsec/parsers"
-            "/etc/crowdsec/scenarios"
-          ];
-          SupplementaryGroups = [
-            "crowdsec"
-            "systemd-journal"
-          ];
-          StateDirectory = "crowdsec";
+        sops.secrets.${cfg.sopsLapiCredentialsFile} = {
+          format = "yaml";
+          mode = "0640";
+          group = "crowdsec";
+          restartUnits = ["crowdsec.service" "crowdsec-lapi.service"];
         };
-      };
 
-      networking.nftables.ruleset = mkBefore ''
-        table inet firewall {
-          set crowdsec-blacklists {
-            type ipv4_addr
-            flags timeout
-          }
-          set crowdsec6-blacklists {
-            type ipv6_addr
-            flags timeout
-          }
-        }
-      '';
+        sops.secrets.${cfg.sopsBouncerCredentialsFile} = {
+          format = "yaml";
+          mode = "0640";
+          group = "crowdsec";
+          restartUnits = ["cs-firewall-bouncer.service"];
+        };
 
-      networking.nftables.firewall.zones.crowdsec-ban = {
-        ingressExpression = [
-          "ip saddr @crowdsec-blacklists"
-          "ip6 saddr @crowdsec6-blacklists"
-        ];
-        egressExpression = [
-          "ip daddr @crowdsec-blacklists"
-          "ip6 daddr @crowdsec6-blacklists"
-        ];
-      };
-      networking.nftables.firewall.rules.crowdsec-ban = {
-        from = ["crowdsec-ban"];
-        to = "all";
-        ruleType = "ban";
-        extraLines = [
-          "counter drop"
-        ];
-      };
+        systemd.services.crowdsec-agent = let
+          preStartScript = pkgs.writeScript "crowdsec-agent-pre-start" ''
+            #!/bin/sh
+            set -e
 
-      systemd.services.cs-firewall-bouncer = let
-        configFile = yaml.generate "cs-firewall-bouncer.yaml" {
-          mode = "nftables";
-          log_mode = "stdout";
-          update_frequency = "10s";
-          api_url = "https://${cfg.lapiDomain}/";
-          nftables = {
-            ipv4 = {
-              set-only = true;
-              table = "firewall";
-            };
-            ipv6 = {
-              set-only = true;
-              table = "firewall";
-            };
+            rm -rf /etc/crowdsec/{parsers,scenarios}/* || true
+            # mkdir -p /etc/crowdsec/{parsers,scenarios}
+
+            ${pkgs.crowdsec}/bin/cscli parsers install ${escapeShellArgs cfg.parsers}
+
+            ${pkgs.crowdsec}/bin/cscli scenarios install ${escapeShellArgs cfg.scenarios}
+          '';
+        in {
+          description = "Crowdsec agent";
+          wantedBy = ["multi-user.target"];
+
+          restartTriggers = [(builtins.toJSON etcConfig)];
+
+          serviceConfig = {
+            Type = "notify";
+            ExecStartPre = [
+              "+${pkgs.coreutils}/bin/mkdir -p /etc/crowdsec/{parsers,scenarios}"
+              "!${preStartScript}"
+            ];
+            ExecStart = "${pkgs.crowdsec}/bin/crowdsec";
+            Environment = "LC_ALL=C LANG=C";
+            Restart = "always";
+            RestartSec = 10;
+            DynamicUser = true;
+            ReadWritePaths = [
+              "/etc/crowdsec/parsers"
+              "/etc/crowdsec/scenarios"
+            ];
+            SupplementaryGroups = [
+              "crowdsec"
+              "systemd-journal"
+            ];
+            StateDirectory = "crowdsec";
           };
         };
-        confDir = pkgs.runCommandLocal "cs-firewall-bounder-config" {} ''
-          mkdir $out
-          ln -s ${configFile} $out/config.yaml
-          ln -s ${config.sops.secrets.${cfg.sopsBouncerCredentialsFile}.path} $out/config.yaml.local
-        '';
-      in {
-        wantedBy = ["multi-user.target"];
-        after = ["nftables.service"];
 
-        unitConfig = {
-          PartOf = "nftables.service";
-          ReloadPropagatedFrom = "nftables.service";
+        networking.nftables.ruleset = mkBefore ''
+          table inet firewall {
+            set crowdsec-blacklists {
+              type ipv4_addr
+              flags timeout
+            }
+            set crowdsec6-blacklists {
+              type ipv6_addr
+              flags timeout
+            }
+          }
+        '';
+
+        networking.nftables.firewall.zones.crowdsec-ban = {
+          ingressExpression = [
+            "ip saddr @crowdsec-blacklists"
+            "ip6 saddr @crowdsec6-blacklists"
+          ];
+          egressExpression = [
+            "ip daddr @crowdsec-blacklists"
+            "ip6 daddr @crowdsec6-blacklists"
+          ];
         };
-        serviceConfig = {
-          Type = "notify";
-          NotifyAccess = "all";
-          ExecStart = "${pkgs.cs-firewall-bouncer}/bin/cs-firewall-bouncer -c ${confDir}/config.yaml";
-          ExecReload = "!${config.systemd.package}/bin/systemctl restart cs-firewall-bouncer.service";
-          AmbientCapabilities = [
-            "CAP_NET_ADMIN"
+        networking.nftables.firewall.rules.crowdsec-ban = {
+          from = ["crowdsec-ban"];
+          to = "all";
+          ruleType = "ban";
+          extraLines = [
+            "counter drop"
           ];
-          SupplementaryGroups = [
-            "crowdsec"
-          ];
-          Restart = "always";
-          RestartSec = 10;
-          DynamicUser = true;
+        };
+
+        systemd.services.cs-firewall-bouncer = let
+          configFile = yaml.generate "cs-firewall-bouncer.yaml" {
+            mode = "nftables";
+            log_mode = "stdout";
+            update_frequency = "10s";
+            api_url = "https://${cfg.lapiDomain}/";
+            nftables = {
+              ipv4 = {
+                set-only = true;
+                table = "firewall";
+              };
+              ipv6 = {
+                set-only = true;
+                table = "firewall";
+              };
+            };
+          };
+          confDir = pkgs.runCommandLocal "cs-firewall-bounder-config" {} ''
+            mkdir $out
+            ln -s ${configFile} $out/config.yaml
+            ln -s ${config.sops.secrets.${cfg.sopsBouncerCredentialsFile}.path} $out/config.yaml.local
+          '';
+        in {
+          wantedBy = ["multi-user.target"];
+          after = ["nftables.service"];
+
+          unitConfig = {
+            PartOf = "nftables.service";
+            ReloadPropagatedFrom = "nftables.service";
+          };
+          serviceConfig = {
+            Type = "notify";
+            NotifyAccess = "all";
+            ExecStart = "${pkgs.cs-firewall-bouncer}/bin/cs-firewall-bouncer -c ${confDir}/config.yaml";
+            ExecReload = "!${config.systemd.package}/bin/systemctl restart cs-firewall-bouncer.service";
+            AmbientCapabilities = [
+              "CAP_NET_ADMIN"
+            ];
+            SupplementaryGroups = [
+              "crowdsec"
+            ];
+            Restart = "always";
+            RestartSec = 10;
+            DynamicUser = true;
+          };
         };
       };
-    };
   }
