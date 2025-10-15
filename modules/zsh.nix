314f2dbaa1eb0c76dfd35d74ab3944a8dafea48d
Jan Beinke <git@janbeinke.com>
2025-10-15 20:04:52 +0200

    module/zsh: Switch to starship prompt

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   mkTrivialModule,
   pkgs,
@@ -21,6 +22,17 @@ let
 in
 mkTrivialModule {
 
+  programs.foot.enableZshIntegration = false;
+  programs.starship = {
+    enable = true;
+    presets = [
+      "nerd-font-symbols"
+    ];
+    settings = {
+      nix_shell.impure_msg = "";
+    };
+  };
+
   programs.zsh = {
 
     enable = true;
@@ -35,8 +47,6 @@ mkTrivialModule {
 
       bindkey -e
     '';
-
-    promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
 
     shellAliases = {
       cal = "cal --monday";
@@ -75,12 +85,15 @@ mkTrivialModule {
         _direnv_hook
       }
 
-      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-      # Initialization code that may require console input (password prompts, [y/n]
-      # confirmations, etc.) must go above this block; everything else may go below.
-      if [[ -r ''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh ]] {
-        source ''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh
+      # OSS7 Integration, e.g. for foot
+      _osc7-pwd () {
+        emulate -L zsh # also sets localoptions for us
+        setopt extendedglob
+        local LC_ALL=C
+        printf '\e]7;file://%s%s\e\' ''${(%):-%m} ''${PWD//(#m)([^@-Za-z&-;_~])/%''${(l:2::0:)$(([##16]#MATCH))}}
       }
+
+      _osc7-pwd
 
       export MANPAGER='nvim +Man!'
       export MANWIDTH=999
@@ -116,6 +129,12 @@ mkTrivialModule {
         }
       }
 
+      _chpwd-osc7-pwd () {
+        (( ZSH_SUBSHELL )) || _osc7-pwd
+      }
+      autoload -U add-zsh-hook
+      add-zsh-hook -Uz chpwd _chpwd-osc7-pwd
+
       pastebin () {
         readonly pastebin='https://0x0.st'
         readonly cmdName=$0
@@ -127,10 +146,6 @@ mkTrivialModule {
         [[ $# > 1 ]] && echo $usage && return 1
         [[ ''${1:-} == '-h' || ''${1:-} == '--help' ]] && echo $usage && return 0
         ''${=filer:-cat} ''${1:-} | curl -F'file=@-' $pastebin
-      }
-
-      if [[ -f ~/.p10k.zsh ]] {
-        source ~/.p10k.zsh
       }
 
     '';
