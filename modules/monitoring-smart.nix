491bf7fafbbee6390851b6c45b7a48790d86cbb6
Jan Beinke <git@janbeinke.com>
2026-01-07 01:25:37 +0100

    module/monitoring-smart: Pin listenAddress and apply default relabeler

--- modules/monitoring-smart.nix
+++ modules/monitoring-smart.nix
@@ -5,15 +5,20 @@
 }:
 
 mkTrivialModule {
-  services.prometheus.exporters.smartctl.enable = true;
+  services.prometheus.exporters.smartctl = {
+    enable = true;
+    listenAddress = "127.0.0.1";
+  };
 
-  environment.etc."alloy/smartctl-exporter.alloy".text = ''
+  environment.etc."alloy/smartctl-exporter.alloy".text = let
+    inherit (config.services.prometheus.exporters.smartctl) listenAddress port;
+  in ''
     prometheus.scrape "smartctl" {
       targets = [
-        {"__address__" = "localhost:${toString config.services.prometheus.exporters.smartctl.port}"},
+        {"__address__" = "${listenAddress}:${toString port}"},
       ]
       scrape_interval = "2m"
-      forward_to = [prometheus.remote_write.default.receiver]
+      forward_to = [prometheus.relabel.default.receiver]
     }
   '';
 }
