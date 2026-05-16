7e081c4dcab594e7d62e75f935a082c093eafeb2
Jan Beinke <git@janbeinke.com>
2026-05-16 15:56:45 +0200

    module/acme: Migrate from crendentialsFile to environmentFile

--- wat/modules/acme.nix
+++ wat/modules/acme.nix
@@ -57,7 +57,7 @@ mkModule {
       certs.${cfg.defaultCertName} = {
         inherit (cfg) extraDomainNames;
         dnsProvider = cfg.dnsProvider;
-        credentialsFile = config.sops.secrets.${cfg.sopsCredentialsFile}.path;
+        environmentFile = config.sops.secrets.${cfg.sopsCredentialsFile}.path;
         postRun = mkIf (length cfg.reloadUnits > 0) ''
           systemctl reload-or-restart ${concatStringsSep " " cfg.reloadUnits}
         '';
